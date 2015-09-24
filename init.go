9caf41330bd930cd3cf59b1fd4b24e264ade7754
liudng <liudng@gmail.com>
2015-09-24 22:54:23 +0800

    Set log default format.

---
+++ init.go
@@ -0,0 +1,13 @@
+// Copyright 2014 The zhgo Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package config
+
+import (
+    "log"
+)
+
+func init() {
+    log.SetFlags(log.Lshortfile)
+}
